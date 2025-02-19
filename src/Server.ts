392c61f37a8b94965efbdd0a52de5a9e8aa15974
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 21:36:07 -0300

    config: initial http server

--- src/Server.ts
+++ src/Server.ts
@@ -1,1 +1,15 @@
-import {} from 'fastify'
+import { fastify } from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+    return 'hello world'
+});
+
+app.listen(
+    {
+        port: 3000
+    }
+).then(() => {
+    console.log('Run Server');   
+});
